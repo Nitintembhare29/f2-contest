57a8b33bcf352b5b0fd31b36adf4d9d85826bd69
Nitin Tembhare <nitintembhare340@gmail.com>
2023-02-12 15:58:17 +0530

    finished

--- script.js
+++ script.js
@@ -8,20 +8,43 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(function(student){
+   if(student.marks > 50){
+    console.log(student);
+   }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(student => {
+    if(student.marks > 50){
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  var newStudent = {id:4, name:"susan", age:"20", marks:45};
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter(student => student.marks > 50);
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  var arr2 = [
+    { id: 4, name: "Nitin", age: "18", marks: 80 },
+    { id: 5, name: "Dravid", age: "20", marks: 85 },
+    { id: 6, name: "Amol", age: "19", marks: 75 },
+  ];
+  var resultant = [...arr,...arr2]
+  // In above line we are first coping arr and then arr2
+  console.log(resultant);
 }
